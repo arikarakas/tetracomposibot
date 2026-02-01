e63b2348ef97b5a7fa9b314a6b4cb6bbef99ebe7
Ari Karakas <arikarakas@hotmail.com>
2026-02-01 18:24:56 +0100

    Add Braitenberg robot implementations and update robot initializations

--- config_TP1.py
+++ config_TP1.py
@@ -21,10 +21,19 @@ display_time_stats = True
 # initialization : create and place robots at initial positions (returns a list containing the robots)
 
 import robot_dumb
+import robot_braitenberg_avoider
+import robot_braitenberg_loveWall
+import robot_braitenberg_hateWall
+import robot_braitenberg_loveBot
+import robot_braitenberg_hateBot
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
-    #x_center = arena_size // 2 - particle_box / 2
+    x_center = arena_size // 2 - particle_box / 2
     y_center = arena_size // 2 - particle_box / 2
     robots = []
-    robots.append(robot_dumb.Robot_player(4, y_center, 0, name="My Robot", team="A"))
+    robots.append(robot_braitenberg_avoider.Robot_player(55, y_center, 0, name="Avoider", team="A"))
+    robots.append(robot_braitenberg_loveWall.Robot_player(40, 55, 30, name="Love Wall", team="B"))
+    robots.append(robot_braitenberg_hateWall.Robot_player(x_center, 90, 90, name="Hate Wall", team="C"))
+    robots.append(robot_braitenberg_loveBot.Robot_player(4, y_center, 0, name="Love Bot", team="D"))
+    robots.append(robot_braitenberg_hateBot.Robot_player(90, 49, 180, name="Hate Bot", team="E"))
     return robots
